574887bfd231e03371c5c2b006a88ae752e2d9a9
Bunajirou <84072744+Bunajirou@users.noreply.github.com>
2021-05-19 20:53:17 +0900

    Add files via upload

---
+++ euclid.py
@@ -0,0 +1,36 @@
+print('ax + bx = gcd(a,b)')
+
+a = int(input('a='))
+b = int(input('b='))
+a2 = a
+b2 = b
+
+print('')
+
+r = a  #  次のループを動かすためのとりあえず代入
+i = 0  #  式の番号
+
+while( r != 0 ):
+    q = a // b
+    r = a % b
+    
+    #print('(',i,')',a,'=',q,'*',b,'+',r)
+    a = b
+    b = r
+    i += 1
+    
+print('GCD=',a)
+
+b = b2 / a
+a = a2 / a
+x = 1
+r = 0  #  次のループを動かすための0代入
+
+while( r != 1 ):
+    x += 1
+    r = (a * x) % b
+    
+y = (1 - a * x ) / b
+
+print('x0 =',x)
+print('y0 =','{:.0f}'.format(y))
