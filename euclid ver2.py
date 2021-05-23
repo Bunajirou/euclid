e607ffe99ba96577745206f83d37ca67825bf348
Bunajirou <s2032052cz@s.chibakoudai.jp>
2021-05-23 23:29:28 +0900

    とりあえずの完成

--- euclid ver2.py
+++ euclid ver2.py
@@ -2,12 +2,16 @@ print('ax + bx = gcd(a,b)')
 
 a = int(input('a='))
 b = int(input('b='))
+a_o = a  #  ax + bx = gcd(1a,b)を出力するためにおいてあるけど
+b_o = b  #  正直なところ邪魔でしかない
+swap_flag = 0
 
 if( b > a ):
     tmp = a
     a = b
-    b = a
-#  a,bが互いに素であるとき、ここから必要ない--------------------------------------------------
+    b = tmp
+    swap_flag = 1
+#  a,bが互いに素であるとき、ここから必要ない↓↓↓--------------------------------------------------
 a_org = a
 b_org = b
 
@@ -26,15 +30,14 @@ while( r != 0 ):
     i += 1
 gcd = a    
 
-print('GCD=',gcd,'\n')
-#  a,bが互いに素であるとき、ここまで必要ない--------------------------------------------
+#print('GCD=',gcd,'\n')
+#  a,bが互いに素であるとき、ここまで必要ない↑↑↑--------------------------------------------
+print('{0}x + {1}y = {2}'.format(a_o, b_o, gcd))  #  これどうしよう
 
 a = a_org // gcd
 b = b_org // gcd
 a_org = a
 b_org = b
-
-print('{0}x + {1}y = 1\n'.format(a, b))
 
 r = 1
 i = 0
@@ -44,27 +47,32 @@ while( r != 0 ):
     q = a // b
     r = a % b
 
-    print('({0}) {1} = {2} * {3} + {4}'.format(i, a, q, b, r))
+    #print('({0}) {1} = {2} * {3} + {4}'.format(i, a, q, b, r))
     qlist.append(q)
     a = b
     b = r
     i += 1
 
-print(qlist)
-
 n = i - 2
-x1 = qlist[n]
-x2 = x1
-x3 = 1
+y = qlist[n]
+x2 = y
+x = 1
 
 for j in range(n, 0, -1):
-    x1 = x1 * qlist[j-1] + x3
+    y = y * qlist[j-1] + x
     tmp = x2
-    x2 = x1
-    x3 = tmp
-    #print(x1,x2,x3)
+    x2 = y
+    x = tmp
 
-x3 = -x3
+if( i % 2 == 1):  #  式の数が奇数ならx,偶数ならyをマイナスにする
+    x = -x
+else:
+    y = -y
 
-print("x0 = {0}, y0 = {1}\n".format(x3, x1))
-print("x = {0} + {1}t, y = {2} - {3}t\n".format(x3, b_org, x1, a_org))
+print("")
+if( swap_flag == 0):  #  a,bを入れ替えたかどうかの場合分け
+    print('x0 = {0}, y0 = {1}'.format(x, y))
+    print('x = {0} + {1}t, y = {2} - {3}t\n'.format(x, b_org, y, a_org))
+else:
+    print('x0 = {0}, y0 = {1}'.format(y, x))
+    print('x = {0} - {1}t, y = {2} + {3}t\n'.format(y, a_org, x, b_org))
